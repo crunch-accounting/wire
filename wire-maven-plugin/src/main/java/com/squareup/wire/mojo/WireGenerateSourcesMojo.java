09cffcc0bd101b4a800acf058de6e72394997182
Andrew Regan <aregan@gmail.com>
2020-05-06 16:29:04 +0100

    Use getters

--- wire-maven-plugin/src/main/java/com/squareup/wire/mojo/WireGenerateSourcesMojo.java
+++ wire-maven-plugin/src/main/java/com/squareup/wire/mojo/WireGenerateSourcesMojo.java
@@ -90,16 +90,16 @@ public class WireGenerateSourcesMojo extends AbstractMojo {
               .withCompact(emitCompact)
               .withProfile(profile);
 
-      for (ProtoFile protoFile : schema.protoFiles()) {
-        if (!protoFilesList.isEmpty() && !protoFilesList.contains(protoFile.location().getPath())) {
+      for (ProtoFile protoFile : schema.getProtoFiles()) {
+        if (!protoFilesList.isEmpty() && !protoFilesList.contains(protoFile.getLocation().getPath())) {
           continue; // Don't emit anything for files not explicitly compiled.
         }
 
-        for (Type type : protoFile.types()) {
+        for (Type type : protoFile.getTypes()) {
           Stopwatch stopwatch = Stopwatch.createStarted();
           TypeSpec typeSpec = javaGenerator.generateType(type);
           ClassName javaTypeName = javaGenerator.generatedTypeName(type);
-          writeJavaFile(javaTypeName, typeSpec, type.location().withPathOnly());
+          writeJavaFile(javaTypeName, typeSpec, type.getLocation().withPathOnly());
           getLog().info(String.format("Generated %s in %s", javaTypeName, stopwatch));
         }
       }
@@ -145,8 +145,8 @@ public class WireGenerateSourcesMojo extends AbstractMojo {
 
   private int countTypes(Schema prunedSchema) {
     int result = 0;
-    for (ProtoFile protoFile : prunedSchema.protoFiles()) {
-      result += protoFile.types().size();
+    for (ProtoFile protoFile : prunedSchema.getProtoFiles()) {
+      result += protoFile.getTypes().size();
     }
     return result;
   }
@@ -164,7 +164,7 @@ public class WireGenerateSourcesMojo extends AbstractMojo {
     Schema schema = schemaLoader.load();
 
     getLog().info(String.format("Loaded %s proto files in %s",
-            schema.protoFiles().size(), stopwatch));
+            schema.getProtoFiles().size(), stopwatch));
 
     return schema;
   }
